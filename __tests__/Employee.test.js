d7f1aa8c06a015f5422ec26856d0f64c8e767bc8
melissikiss <melissikiss@gmail.com>
2020-11-20 16:54:45 -0800

    tested and passed methods for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,3 +9,26 @@ test('creates a Employee', () => {
     expect(employee.role).toEqual(expect.any(String));
 });
 
+
+test('gets employee name', () => {
+    const employee = new Employee('Jared', 5, 'jared@gmail.com');
+    expect(employee.getName()).toEqual(expect.stringContaining('Jared'));
+})
+
+
+test('gets employee id', () => {
+    const employee = new Employee('Jared', 5, 'jared@gmail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+
+test('gets employee email', () => {
+    const employee = new Employee('Jared', 5, 'jared@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining("@gmail.com"));
+})
+
+
+test('gets role of employee', () => {
+    const employee = new Employee('Jared', 5, 'jared@gmail.com');
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
+})
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -8,4 +8,10 @@ test("testing to see if Manager is created correctly", () => {
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.role).toEqual(expect.any(String));
     expect(employee.officeNumber).toEqual(expect.any(Number));
+});
+
+test('gets role of employee', () => {
+    const employee = new Manager("Kyle", 9, 'kyle@gmail.com', 7);
+
+    expect(employee.getRole()).toEqual(expect.stringContaining('Manager'));
 })
